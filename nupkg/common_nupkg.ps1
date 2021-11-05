f96b4323548c04f6906422c5a1918aed7e2245a1
tanyongzheng <tanyongzheng@126.com>
2021-11-05 17:01:30 +0800

    feat:添加CI文件

---
+++ nupkg/common_nupkg.ps1
@@ -0,0 +1,15 @@
+# Paths
+$packFolder = (Get-Item -Path "./" -Verbose).FullName
+$rootFolder = Join-Path $packFolder "../"
+
+# List of solutions
+$solutions = (
+	"AbpvNext.MyCultureMap"
+)
+
+# List of projects
+$projects = (
+
+    # ÏîÄ¿
+    "AbpvNext.MyCultureMap"
+)
